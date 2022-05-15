1675e3ac6acbff2d85d33eb4b2c37a4c55a4c2cf
brnborges <brn5haxx@gmail.com>
2022-05-15 15:39:03 +0100

    commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
 const resumeLink =
-  'https://raw.githubusercontent.com/brnborges/Portfolio2022/main/BrunoResume.pdf'
+  'https://raw.githubusercontent.com/brnborges/Portfolio2022/master/src/Assets/BrunoResume.pdf'
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200)
