14e12fcdf3553d897f8ab2b8b377e152f2515332
brnborges <brn5haxx@gmail.com>
2023-03-22 12:39:18 +0000

    projects update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,9 @@ import Particle from '../Particle'
 import narutoGame from '../../Assets/Projects/narutogame1.png'
 import houseFinder1 from '../../Assets/Projects/housefinder1.png'
 import houseFinder5 from '../../Assets/Projects/housefinder5.png'
+import weatherApp from '../../Assets/Projects/weatherApp.png'
+import movieApp from '../../Assets/Projects/movieApp.png'
+import typeApp from '../../Assets/Projects/typeApp.png'
 import doctorCare from '../../Assets/Projects/web1.png'
 
 function Projects() {
@@ -55,6 +58,36 @@ function Projects() {
               title="Naruto Game"
               description="Naruto-Makioutsu is a 3d game developed in C# to hit the target of one of the most requested foods by the anime character Naruto"
               link="https://github.com/brnborges/CA2-20053-Naruto-Makioutsu-Game"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={typeApp}
+              isBlog={false}
+              title="Type Test"
+              description="Enhance your typing speed with this app! Built with HTML, CSS & JS to test and improve your typing skills. Get real-time feedback and progress tracking."
+              link="https://type-test-app.vercel.app"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={weatherApp}
+              isBlog={false}
+              title="Weather Forecast"
+              description="Get up-to-date weather info with this app! HTML, CSS & JS combined to call external API for accurate forecasts."
+              link="https://bruno-weather.vercel.app"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={movieApp}
+              isBlog={false}
+              title="Movie Rating"
+              description="Discover top-rated movies with this app! Built with HTML, CSS & JS to call external API for reliable ratings and user reviews. Start streaming now!"
+              link="https://bruno-movie.vercel.app"
             />
           </Col>
         </Row>
