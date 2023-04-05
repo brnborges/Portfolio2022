4f0e6694ccb643c03c6185d551307b4e5c9805a6
brnborges <brn5haxx@gmail.com>
2023-04-05 18:17:01 +0100

    FIxing footer

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import myImg from '../../Assets/avatar.svg'
 import Tilt from 'react-parallax-tilt'
-import { AiFillGithub, AiOutlineTwitter, AiFillInstagram } from 'react-icons/ai'
+import { AiFillGithub } from 'react-icons/ai'
 import { FaLinkedinIn } from 'react-icons/fa'
 
 function Home2() {
